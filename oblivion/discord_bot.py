b90ed7833d4fae598831c4ae66cfa1d01828bf64
Jake <jgjedi@gmail.com>
2024-02-12 17:10:14 -0700

    testing discord commands

--- oblivion/discord_bot.py
+++ oblivion/discord_bot.py
@@ -20,12 +20,24 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Event listener for when the bot has switched from offline to online.
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+	print(f'Logged in as {bot.user.name}')
+
+
+@bot.event
+async def on_message(message):
+	# Prevent bot from responding to its own messages
+	if message.author == bot.user:
+		return
+
+	# A simple command that responds when someone types "hello"
+	if message.content.lower() == 'hello':
+		await message.channel.send('Hello!')
+
 
 # A simple command that responds with 'Hello!' when someone types '!hello'
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello!')
+# @bot.command()
+# async def hello(ctx):
+#	 await ctx.send('Hello!')
 
 # Replace 'YOUR_BOT_TOKEN_HERE' with your bot's token
 bot.run(bot_token)
