037a0c50eecda19e7d599accc7162bde3c44c174
Jake <jgjedi@gmail.com>
2024-02-15 14:40:18 -0700

    changed to bot

--- discord_bot.py
+++ discord_bot.py
@@ -21,13 +21,13 @@ intents.guilds = True
 intents.guild_messages = True  # Enable guild messages
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-client = discord.Client(intents=intents)
+#client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
 	try:
-		print(f'Logged in as {client.user.name}')
+		print(f'{client.user.name} Online')
 	except Exception as e:
 		print(f'ISSUE: {e}')
 
@@ -41,29 +41,35 @@ async def on_message(message):
 	if message.content.lower() == 'hello':
 		await message.channel.send(f'Hello! {client.user}')
 
-@bot.command()
-async def retrieve_random_plant(message):
+@bot.command(aliases=['hello'])
+async def greet(ctx):
+	await ctx.send("Hello!")
+
+
+
+@bot.command(aliases=['pick'])
+async def pick_random_plant(message):
 	if message.author == client.user:
 		return
-	if message.content == 'pick':
-		plant_factory = PlantFactory()
-		plt = plant_factory.get_random_plant()
-		plt_name = plt[0]
+	# if message.content == 'pick':
+	plant_factory = PlantFactory()
+	plt = plant_factory.get_random_plant()
+	plt_name = plt[0]
 
-		embed = discord.Embed(
-			title=f'{plt.name}',
-			description='Need to put text here about the flower desciption - maybe more if you roll well',  # Description or the main text content
-			color=discord.Color.blue()  # Color of the side strip of the embed
-		)
-		embed.set_image(url='https://en.uesp.net/wiki/File:OB-icon-ingredient-Arrowroot.png')
-		embed.add_field(name='Effect 1', value='Restore Strength', inline=False)
-		embed.add_field(name='Effect 2', value='Water Breathing', inline=False)
-		embed.add_field(name='Effect 3', value='Silence', inline=False)
-		embed.add_field(name='Effect 4', value='Light', inline=False)
-		await ctx.send(embed=embed)
+	embed = discord.Embed(
+		title=f'{plt.name}',
+		description='Need to put text here about the flower desciption - maybe more if you roll well',  # Description or the main text content
+		color=discord.Color.blue()  # Color of the side strip of the embed
+	)
+	embed.set_image(url='https://en.uesp.net/wiki/File:OB-icon-ingredient-Arrowroot.png')
+	embed.add_field(name='Effect 1', value='Restore Strength', inline=False)
+	embed.add_field(name='Effect 2', value='Water Breathing', inline=False)
+	embed.add_field(name='Effect 3', value='Silence', inline=False)
+	embed.add_field(name='Effect 4', value='Light', inline=False)
+	await ctx.send(embed=embed)
 		#await message.channel.send(embed=embed)
 
-client.run(bot_token)
+# client.run(bot_token)
 bot.run(bot_token)
 
 
